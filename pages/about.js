0485b8e7cf378e1f5e6021a931982fba4f15902b
Abraham Camargo <161561158+Suberror27@users.noreply.github.com>
2024-10-08 00:27:21 -0500

    Update about.js

--- pages/about.js
+++ pages/about.js
@@ -3,7 +3,7 @@ export default function About() {
     return (
     <>
     <div className="fade-in">
-      <h1 className="text-gray-700 font-semibold text-2xl md:text-5xl text-center pb-10">About Me</h1>
+      <h1 className="text-gray-700 font-semibold text-2xl md:text-5xl text-center pb-10">About Me.</h1>
     </div>
     </>
     );
